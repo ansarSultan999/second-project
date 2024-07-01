9330742c507ebe5e8708285d7165734881ff15bb
Your Name <you@example.com>
2024-07-01 17:05:53 +0500

    today work is done

---
+++ second-project/assets/js/app.js
@@ -0,0 +1,10 @@
+$('.humburger > i').click(() => {
+    $(".navside-1").toggleClass('showbar');
+});
+
+
+// $("span.menu_plus").click(function() {
+//     let parent = $(this).parent();
+//     parent.siblings().find('ul').slideUp();
+//     $(this).siblings("ul").slideToggle();
+// });
